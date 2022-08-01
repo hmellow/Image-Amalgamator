fff47626682517101f6a4c5d92d8b1579ef9cac9
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-08-01 00:37:40 -0600

    + Opt. Image Scaling

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ const Jimp = require('jimp');
 
 let margin = 5;
 
-async function mergeImages(imgs) {
+async function mergeImages(imgs, writePath, scaleWidth, scaleHeight) {
     let finalWidth = 0;
     let widthArr = [];
     let finalHeight = 0;
 
+    // Calculate dimensions of final image and collect individual widths
     for(let i = 0; i < imgs.length; i++) {
         let image = await Jimp.read(imgs[i]);
 
@@ -20,13 +21,22 @@ async function mergeImages(imgs) {
         }
     }
 
+    let working;
+    // Create a transparent image and paste images on top of it
     new Jimp(finalWidth + margin, finalHeight + margin, (err, back) => {
         let currentR = 0;
+
         for(let i = 0; i < imgs.length; i++) {
             Jimp.read(imgs[i], (err, fore) => {
                 back.blit(fore, currentR + margin, 0);
-                back.write("blit.png");
                 currentR += fore.bitmap.width;
+
+                // Scale on the final iteration if enough arguments exist
+                if((i == imgs.length - 1) && (arguments.length == 4)) {
+                    back.scaleToFit(scaleWidth, scaleHeight);
+                }
+
+                back.write(writePath);
             })
         }
     });
